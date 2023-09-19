79743df6bc71923a4204873eb69c65bd4130b688
_ <4256466+leehart@users.noreply.github.com>
2023-09-19 10:35:50 +0100

    Try other conf paths

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,9 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../..')) # local
+sys.path.insert(0, os.path.abspath('..')) # CI ?
+sys.path.insert(0, os.path.abspath('.')) # CI ?
 
 
 # -- Project information -----------------------------------------------------
